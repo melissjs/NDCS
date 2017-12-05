0146583e17600781220491b7bead66862637a68c
Melissa Schwartz <melissjs@gmail.com>
2017-12-04 18:52:11 -0800

    authentication and user

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var bcrypt = require('bcryptjs');
-
+var jwt = require('jsonwebtoken');
 // Models
 var User = require('../models/user');
 
@@ -22,7 +22,7 @@ router.get('/', function(req, res, next) {
     });
 });
 
-/* POST USER */
+/* CREATE NEW USER */
 router.post('/add', function(req, res, next) {
   let user = new User({
     username: req.body.username,
@@ -42,5 +42,34 @@ router.post('/add', function(req, res, next) {
   });
 });
 
+/* SIGN IN USER */
+router.post('/signin', function(req, res, next) {
+  User.findOne({username: req.body.username}, function(err, user){
+    if (err) {
+      return res.status(500).json({
+        title: 'An error occured',
+        error: err
+      });
+    }
+    if (!user) {
+      return res.status(401).json({
+        title: 'Login failed',
+        error: {message: 'Invalid login credentials'}
+      });
+    }
+    if (!bcrypt.compareSync(req.body.password, user.password)) {
+      return res.status(401).json({
+        title: 'Login failed',
+        error: {message: 'Invalid login credentials'}
+      });
+    }
+    var token = jwt.sign({user: user}, 'secret', {expiresIn: 7200});
+    res.status(200).json({
+      message: 'Successfully logged in',
+      token: token,
+      userId: user._id
+    });
+  })
+})
 
 module.exports = router;
