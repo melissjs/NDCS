4d4ee54e71c64b3c26fe9899a1d8b47e4efbea36
Melissa Schwartz <melissjs@gmail.com>
2017-12-26 18:34:53 -0800

    models before check

--- models/affidavit.js
+++ models/affidavit.js
@@ -4,14 +4,14 @@ const Schema = mongoose.Schema;
 const affidavitSchema = new Schema({
   firstName: { type: String, required: [true, 'First name required'], minlength: [2, 'First name must be at least two characters'] },
   lastName: { type: String, required: [true, 'Last name required'], minlength: [2, 'Last name must be at least two characters'] },
-  consent: { type: Boolean, required: true },
+  consent: { type: Boolean, required: [true, 'Consent required'] },
   streetAddress: { type: String, required: [true, 'Address required'] },
   zip: { type: Number, required: [true, 'Zip code required'] },
   emailAddress: { type: String, lowercase: true, trim: true },
   comments: String,
   volunteerId: { type: Schema.Types.ObjectId, ref: 'User', required: [true, 'VolunteerId required'] },
   electionId: { type: Schema.Types.ObjectId, ref: 'Election', required: [true, 'ElectionId required'] },
-  timestamp: { type: Date, default: Date.now }
+  timestamp: { type: Date, default: Date.now, required: [true, 'Timestamp required'] }
 });
 
 module.exports = mongoose.model('Affidavit', affidavitSchema);
