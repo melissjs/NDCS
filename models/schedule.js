7c2700e26b421043118cf358dae17862254ea28a
Melissa Schwartz <melissjs@gmail.com>
2018-01-16 17:20:00 -0800

    joinhistory and reviews added to schedule

--- models/schedule.js
+++ models/schedule.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const TimesheetSchema = require('../schemas/timesheet');
+const JoinhistorySchema = require('../schemas/joinhistory');
 
 function ShiftValidator(shiftArray) {
 	return shiftArray.every((shift)=>{
@@ -11,6 +12,7 @@ function ShiftValidator(shiftArray) {
 const ScheduleSchema = new Schema({
   userId: { type: Schema.Types.ObjectId, ref: 'User', required: [true, 'UserId required'] },
   auditId: { type: Schema.Types.ObjectId, ref: 'Audit', required: [true, 'AuditId required'] },
+  joinHistory:  { type: [JoinhistorySchema], required: [true, 'JoinHistory required'] },
   shifts: { type: [Number], validate: {validator: ShiftValidator, message: 'Invalid shift options' } },
   timeSheet: { type: [TimesheetSchema] },
   exitReview: { type: Schema.Types.ObjectId, ref: 'Exitreview' },
@@ -22,7 +24,7 @@ const ScheduleSchema = new Schema({
 // }
 );
 
-//////////// TO DO
+//////////// TO DO //////////////
 // showedUp: { type: Boolean, default: false }
 // completedAllShifts: { type: Boolean, default: false }
 
