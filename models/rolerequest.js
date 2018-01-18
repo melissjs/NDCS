ec69f3196d45b670b7fee48e113fead32bece03b
Melissa Schwartz <melissjs@gmail.com>
2018-01-17 19:29:19 -0800

    role request model

---
+++ models/rolerequest.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const TimesheetSchema = require('../schemas/timesheet');
+const JoinhistorySchema = require('../schemas/joinhistory');
+
+let ethnicities = [ 'volunteer', 'auditor', 'lead', 'admin'];
+function ethnicitiesValidator(ethnicitiesArray) {
+  return ethnicitiesArray.every((ethnicity)=>{
+    return (ethnicities.includes(ethnicity))  
+  })
+};
+
+const RolerequestSchema = new Schema({
+  userId: { type: Schema.Types.ObjectId, ref: 'User', required: [true, 'UserId required'] },
+  auditId: { type: Schema.Types.ObjectId, ref: 'Audit', required: [true, 'AuditId required'] },
+  rolesRequested: { type: [String], validate: { validator: ethnicitiesValidator, message: "Role does not exist" } },
+  shifts: { type: [Number], validate: {validator: ShiftValidator, message: 'Invalid shift options' } },
+  timeSheet: { type: [TimesheetSchema] },
+  exitReview: { type: Schema.Types.ObjectId, ref: 'Exitreview' },
+  auditorReviews: { type: [Schema.Types.ObjectId], ref: 'Auditorreview' },
+});
+
+module.exports = mongoose.model('Rolerequest', RolerequestSchema);
+
+
