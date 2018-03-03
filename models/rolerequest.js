9822505357832fddcbfa7f93187f8fcb243ce1c4
Melissa Schwartz <melissjs@gmail.com>
2018-03-02 20:52:29 -0800

    role request design pattern

--- models/rolerequest.js
+++ models/rolerequest.js
@@ -5,18 +5,26 @@ const JoinhistorySchema = require('../schemas/joinhistory');
 
 let roles = [ 'volunteer', 'auditor', 'lead', 'admin'];
 function rolesValidator(rolesArray) {
-  return rolesArray.every((ethnicity)=>{
-    return (roles.includes(ethnicity))  
+  return rolesArray.every((role)=>{
+    return (roles.includes(role))  
   })
 };
 
 const RolerequestSchema = new Schema({
   userId: { type: Schema.Types.ObjectId, ref: 'User', required: [true, 'UserId required'] },
-  rolesRequested: { type: [String], validate: { validator: rolesValidator, message: "Role does not exist" } },
-  shifts: { type: [Number], validate: {validator: ShiftValidator, message: 'Invalid shift options' } },
-  timeSheet: { type: [TimesheetSchema] },
-  exitReview: { type: Schema.Types.ObjectId, ref: 'Exitreview' },
-  auditorReviews: { type: [Schema.Types.ObjectId], ref: 'Auditorreview' },
+  roleRequested: { type: [String], validate: { validator: rolesValidator, message: "Role does not exist" } },
+  // reason-role:,
+  // questions:
+  // preferredContact:,
+  // references:,
+  // facebook:,
+  // twitter:,
+  // instagram:,
+  // linkedin:,
+  // website:
+  // resume:,
+  // otherLinks:,
+
 });
 
 module.exports = mongoose.model('Rolerequest', RolerequestSchema);
