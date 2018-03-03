2f1985ef47c6cdcbedb39ec6076bf89c793ce205
Melissa Schwartz <melissjs@gmail.com>
2018-03-03 14:07:10 -0800

    roleRequest

--- models/rolerequestapplication.js
+++ models/rolerequestapplication.js
@@ -4,15 +4,17 @@ const Schema = mongoose.Schema;
 const RoleRequestApplicationSchema = new Schema({
   userId: { type: Schema.Types.ObjectId, ref: 'User', required: [true, 'UserId required'] },
   roleRequests: { type: [RoleRequestSchema] },
-  // preferredContact:,
-  // references:,
-  // facebook:,
-  // twitter:,
-  // instagram:,
-  // linkedin:,
-  // website:
-  // resume:,
-  // otherLinks:,
+  preferredContact:{ type: String, required: [true, 'Preferred contact required'] },
+  references: { type: String },
+  facebook: { type: String },
+  twitter: { type: String },
+  instagram: { type: String },
+  linkedin: { type: String },
+  website: { type: String },
+  resume: { type: String },
+  areasOfExpertise: { type: String },
+  relatedExperience: { type: String },
+  otherLinks: { type: String }
 });
 
 module.exports = mongoose.model('Rolerequest', RoleRequestApplicationSchema);
