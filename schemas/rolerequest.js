0872750b158683cbf078f810321727279868de51
Melissa Schwartz <melissjs@gmail.com>
2018-03-03 19:52:28 -0800

    roleRequest

--- schemas/rolerequest.js
+++ schemas/rolerequest.js
@@ -5,7 +5,7 @@ const RoleRequestSchema = new Schema({
 roleRequested: { type: String, required: [true, 'Role required'], enum: {values:  ['user', 'volunteer', 'auditor', 'watcher', 'lead', 'legalObserver', 'admin'], message: 'Invalid role option' } },
 reasons: { type: String, required: [true, 'Role reason required']},
 questions: { type: String },
-reviewed: { type: Boolean,  default: false, required: [true, 'reviewed required'] }
+status: { type: String, required: [true, 'Status required'], default: 'unreviewed', enum: {values:  ['unreviewed', 'approved', 'denied'], message: 'Invalid role option' } },
 });
 
 module.exports = RoleRequestSchema;
