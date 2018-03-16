2cee3bc452aed5ba6fd10771e373368ecfcd6df2
Melissa Schwartz <melissjs@gmail.com>
2018-03-15 21:45:24 -0700

    rm array from date on authSchema

--- schemas/auth.js
+++ schemas/auth.js
@@ -22,7 +22,7 @@ const Schema = mongoose.Schema;
 const AuthSchema = new Schema({
   // authenticatingUserId: { type: Schema.Types.ObjectId, ref: 'User', required: [true, 'AuthenticatingUserId required'], validate: { validator: authValidator, message: 'User cannot authenticate self' } },
   authenticatingUserId: { type: Schema.Types.ObjectId, ref: 'User', required: [true, 'AuthenticatingUserId required'] },
-  date: { type: [Date], required: [true, 'Authentication date required'] }
+  date: { type: Date, required: [true, 'Authentication date required'] }
 });
 
 module.exports = AuthSchema;
