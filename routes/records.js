5dc90443182b12306a9fbed1d600b29362c866d9
Melissa Schwartz <melissjs@gmail.com>
2018-03-25 21:19:33 -0700

    records route

--- routes/records.js
+++ routes/records.js
@@ -1,5 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var bcrypt = require('bcryptjs');
+var jwt = require('jsonwebtoken');
+var Pollingstation = require('../models/pollingstation');
+var Election = require('../models/election');
+var Audit = require('../models/audit');
+var User = require('../models/user');
+var Schedule = require('../models/schedule');
+const mongoose = require('mongoose');
 
 // Models
 var Affidavit = require('../models/affidavit');
@@ -9,9 +17,40 @@ var Demographics = require('../models/demographics');
 var Officevote = require('../models/officevote');
 var Vote = require('../models/vote');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a records resource');
+// ------------------- AUTH WITH JWT -------------------
+
+/* VALIDATE AUTHORIZATION HEADER THEN APPEND USER TO REQ */
+router.use('/', function(req, res, next) {
+  let token = req.headers.authorization.split(' ')[1]
+  jwt.verify(token, 'secret', function(err, decoded) {
+    if (err) {
+      return res.status(401).json({
+        title: 'Not authenticated',
+        error: err
+      });
+    }
+    req.authedUser = decoded.user;
+    next();
+  });
+});
+
+// ------------------- GET -------------------
+
+/* GET ALL AUDITS */
+router.get('/all', function(req, res, next) {
+  Audit.find({})
+    .exec(function(err, audits) {
+      if (err) {
+        return res.status(500).json({
+          title: 'An error occurred',
+          error: err
+        });
+      }
+      res.status(200).json({
+        message: 'Success',
+        obj: audits
+      });
+    });
 });
 
 module.exports = router;
